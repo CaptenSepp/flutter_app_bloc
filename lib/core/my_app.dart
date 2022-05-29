4962a2590c74b89d61176fbcd97341490416061a
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-30 00:33:47 +0200

    global generated routing added

--- lib/core/my_app.dart
+++ lib/core/my_app.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../cubit/counter_cubit.dart';
+
 import 'router/app_router.dart';
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
+    return BlocProvider<CounterCubit>(
       create: (context) => CounterCubit(),
       child: MaterialApp(
         title: 'Flutter Bloc',
@@ -26,11 +23,5 @@ class _MyAppState extends State<MyApp> {
         onGenerateRoute: _appRouter.onGenerateRoute,
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,39 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_app_bloc/screens/second_screen.dart';
 
-import '../../cubit/counter_cubit.dart';
 import '../../screens/home_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
   Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: const HomeScreen(
-              title: 'HomeScreen',
-            ),
+          builder: (_) => const HomeScreen(
+            title: 'HomeScreen',
           ),
         );
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: const HomeScreen(
-              title: 'HomeScreen',
-            ),
+          builder: (_) => const SecondScreen(
+            title: 'SecondScreen',
           ),
         );
 
       default:
     }
     return null;
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
