21dd4bff5c0ac2f469cc405e3996ad58b71b7e4d
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-30 00:56:00 +0200

    internetCubit class edited

--- lib/cubit/internet_cubit.dart
+++ lib/cubit/internet_cubit.dart
@@ -1,13 +1,22 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:meta/meta.dart';
 
 part 'internet_state.dart';
 
 class InternetCubit extends Cubit<InternetState> {
-  InternetCubit() : super(InternetLoading());
+  final Connectivity connectivity;
 
+  late StreamSubscription connectionStreamSubscription;
 
-  
+  InternetCubit({required this.connectivity}) : super(InternetLoading()) {
+    connectionStreamSubscription =
+        connectivity.onConnectivityChanged.listen((connectivityResult) {});
+  }
+  void emitInternetConnected(ConnectionType connectionType) =>
+      emit(InternetConnected(connectionType: connectionType));
+
+  void emitInternetDisconnected() => emit(InternetDisconnected());
 }
