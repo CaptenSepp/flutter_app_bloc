aea9437c4ca7e7d9ece62f9c3c8d3405bafe5cd4
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-30 00:33:21 +0200

    internet connectivity bloc added

---
+++ lib/cubit/internet_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:meta/meta.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  InternetCubit() : super(InternetLoading());
+
+
+  
+}
---
+++ lib/cubit/internet_state.dart
@@ -0,0 +1,18 @@
+part of 'internet_cubit.dart';
+
+@immutable
+abstract class InternetState {}
+
+class InternetLoading extends InternetState {}
+
+class InternetConnected extends InternetState {
+  final ConnectionType connectionType;
+
+  InternetConnected({
+    required this.connectionType,
+  });
+}
+
+class InternetDisconnected extends InternetState {}
+
+class ConnectionType {}
